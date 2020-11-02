d6715292a4446f2656a66b95f761d38badc7ed43
ElizabethLS <69877268+ElizabethLS@users.noreply.github.com>
2020-11-01 23:07:41 -0500

    message about what you changed

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ma = Marshmallow(app)
 
 db = SQLAlchemy(app)
 
+# ROUTES
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('base.html')   
+    todos = Todo.query.all()        # get all the todos from the database
+    return render_template('base.html', todos=todos)     # todos=todos pass this variable 'todos', and use this variable base.html
+
+@app.route('/add', methods = ['POST'])
+def add():
+    title = request.form['title']   # gets the title from the form. request is an object that holds the form data
+    todo = Todo(title=title)        # use the Todo class from line 29 to create a todo and save it in a variable
+
+    db.session.add(todo)            # get the todo ready to save
+    db.session.commit()             # save todo to the database
+    return redirect('/')            # redirect to the main page 
+
+# CLASSES
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(120))
+
+    def __repr__(self):
+        return "Todo('{}')".format(self.title)
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ('titles',)
 
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
+
+
+""" *******
+   1. How to loop over data in the html file. THere is a variable called 'todos' which is an array. How to display that information in the html file.
+     - look for {% %} type stuff when reading about flask
+   2. read those articles i sent, your data is called 'todos'
+""" 
