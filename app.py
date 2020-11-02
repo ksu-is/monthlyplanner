01fc8677f36dd3a7260cb3f5f59701114c926e4a
ElizabethLS <69877268+ElizabethLS@users.noreply.github.com>
2020-11-01 22:21:20 -0500

    added the form

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+@app.route('/', methods = ['GET'])
 def index():
-    return "Hello World"
-
-@app.route('/')
-def about():
-    return"About"    
-
+    return render_template('base.html')   
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
